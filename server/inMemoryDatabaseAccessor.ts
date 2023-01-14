223c781bf4bbfe47e116cccd754de45a4e5465b2
puolimatkankrouvi <cde_1414@yahoo.com>
2023-01-14 11:09:14 +0200

    Test private member on in memory database accessor

--- server/inMemoryDatabaseAccessor.ts
+++ server/inMemoryDatabaseAccessor.ts
@@ -2,14 +2,14 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 
 export default class InMemoryDatabaseAccessor {
-    private inMemoryDatabase: MongoMemoryServer | null = null;
+    #inMemoryDatabase: MongoMemoryServer | null = null;
 
     public async getOrCreateInMemoryDatabase(): Promise<MongoMemoryServer> {
-        if (this.inMemoryDatabase) {
-            return Promise.resolve(this.inMemoryDatabase);
+        if (this.#inMemoryDatabase) {
+            return Promise.resolve(this.#inMemoryDatabase);
         }
 
-        this.inMemoryDatabase = await MongoMemoryServer.create();
-        return this.inMemoryDatabase;
+        this.#inMemoryDatabase = await MongoMemoryServer.create();
+        return this.#inMemoryDatabase;
     }
 }
