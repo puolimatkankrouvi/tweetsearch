9cfb7d7e0f88e7a2521183fe1919458137d7ae57
Henri Vekuna <cde_1414@yahoo.com>
2025-08-30 15:23:14 +0300

    Fix build directory

--- react-ui/vite.config.js
+++ react-ui/vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build"
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
