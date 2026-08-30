a53a534ce6f6572e912c4b1506cdd6743c7d2bc1
Henri Vekuna <cde_1414@yahoo.com>
2026-08-30 11:42:40 +0300

    Move to typescript-eslint

--- react-ui/eslint.config.mjs
+++ react-ui/eslint.config.mjs
@@ -1,38 +1,35 @@
 import globals from "globals";
 import eslintJs from "@eslint/js";
-import eslintReactJsx from "eslint-plugin-react-jsx";
-import eslintReactDom from "eslint-plugin-react-dom";
-import eslintReactWebApi from "eslint-plugin-react-web-api";
+import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig(
-  {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+	{
+		files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
 
-    // Extend recommended rule sets from:
-    // 1. ESLint JS's recommended rules
-    // 2. ESLint React's recommended rules
-    extends: [
-      eslintJs.configs.recommended,
-      eslintReactJsx.configs.recommended,
-      eslintReactDom.configs.recommended,
-      eslintReactWebApi.configs.recommended
-    ],
+		// Extend recommended rule sets from:
+		// 1. ESLint JS's recommended rules
+		// 2. typescript-eslint's recommended rules
+		// 3. ESLint React's recommended rules
+		extends: [
+			eslintJs.configs.recommended,
+			...tseslint.configs.recommended,
+		],
 
-    // Configure language/parsing options
-    languageOptions: {
-      // Include browser global variables (window, document, etc.)
-      globals: {
-        ...globals.browser,
-      },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true, // Enable JSX syntax support
-        },
-      },
-    },
+		// Configure language/parsing options
+		languageOptions: {
+			// Include browser global variables (window, document, etc.)
+			globals: {
+				...globals.browser,
+			},
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true, // Enable JSX syntax support
+				},
+			},
+		},
 
-    // Custom rule overrides (modify rule levels or disable rules)
-    rules: {},
-  },
+		// Custom rule overrides (modify rule levels or disable rules)
+		rules: {},
+	},
 );
