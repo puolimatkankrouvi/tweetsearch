beff661932f46e7cbddef24f47b3c05ba4570603
Henri Vekuna <cde_1414@yahoo.com>
2026-08-30 12:26:15 +0300

    Add types reference to api calls

--- react-ui/src/apiCalls.ts
+++ react-ui/src/apiCalls.ts
@@ -1,35 +1,7 @@
+/// <reference types="../../index.d.ts" />
+
 import Axios, { type AxiosResponse, AxiosError } from "axios";
 import type { RawSearchResult, OldSearch, SearchWithTweets, Tweet } from "./types";
-
-declare namespace TweetSearch {
-    export namespace Server {
-        interface Tweet {
-            created_at: string;
-            text: string;
-            user: User;
-        }
-        
-        interface User {
-            name: string;
-            screen_name: string;
-            profile_image_url: string;
-        }
-        
-        export interface OldSearchWithoutTweets {
-            id?: string;
-            date: string;
-            name: string;
-        }
-        
-        export interface TweetSearch extends OldSearchWithoutTweets {
-            tweets: ReadonlyArray<Tweet>;
-        }
-
-        export interface ErrorMessage {
-            message: string,
-        }
-    }
-}
 
 const searchUrl = `/api/search/`;
 const oldSearchesUrl = `/api/oldsearches/`;
