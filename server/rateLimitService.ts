7a3b9a0f4cd02d6339d8ebf9a3cd28373ba245d0
Henri Vekuna <cde_1414@yahoo.com>
2026-09-15 13:21:36 +0300

    getResetTime 60min from now

--- server/rateLimitService.ts
+++ server/rateLimitService.ts
@@ -78,7 +78,8 @@ class RateLimitService implements Store {
     }
 
     private getResetTime(): Date {
-        return new Date();
+        // 60 minutes from now.
+        return new Date(Date.now() + 60 * 60 * 1000);
     }
 }
 
