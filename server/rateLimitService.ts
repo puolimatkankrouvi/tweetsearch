c104459bf587cc0a7f66c1ecb9558b4e1ffa7870
Henri Vekuna <cde_1414@yahoo.com>
2026-09-15 08:34:01 +0300

    Started with RateLimitService

---
+++ server/rateLimitService.ts
@@ -0,0 +1,64 @@
+import type {
+    Store,
+    IncrementResponse
+} from "express-rate-limit"
+
+import db from "./db";
+import { RateLimitEntryModel } from "./models/rateLimitEntry";
+
+class RateLimitService implements Store {
+    async increment(key: string): Promise<IncrementResponse> {
+        await db.connect();
+
+        const existingRateLimitEntry = await RateLimitEntryModel.findOne({ key: key }).exec();
+
+        if (!existingRateLimitEntry) {
+            const newRateLimitEntry = new RateLimitEntryModel({
+                key: key,
+                total_hits: 1,
+                reset_time: new Date(),
+            });
+            
+            const rateLimitEntry = await newRateLimitEntry.save();
+
+            return {
+                totalHits: rateLimitEntry.total_hits,
+                resetTime: rateLimitEntry.reset_time,
+            }
+        }
+
+        const newTotalHits = existingRateLimitEntry.total_hits + 1;
+        existingRateLimitEntry.updateOne({
+            total_hits: newTotalHits,
+            reset_time: new Date(),
+        });
+
+        return {
+            totalHits: existingRateLimitEntry.total_hits,
+            resetTime: existingRateLimitEntry.reset_time,
+        }
+    }
+
+    async decrement(key: string): Promise<void> {
+        await db.connect();
+
+        const existingRateLimitEntry = await RateLimitEntryModel.findOne({ key: key }).exec();
+        if (!existingRateLimitEntry) {
+            return;
+        }
+
+        const newTotalHits = existingRateLimitEntry.total_hits - 1;
+        existingRateLimitEntry.updateOne({
+            total_hits: newTotalHits,
+            reset_time: new Date(),
+        });
+    }
+
+    async resetKey(key: string): Promise<void> {
+        await db.connect();
+
+        await RateLimitEntryModel.deleteOne({ key: key }).exec();
+    }
+}
+
+export default RateLimitService;
