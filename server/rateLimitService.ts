a75e9a957eb0174dc565939a350f50e0ac3ca9bc
Henri Vekuna <cde_1414@yahoo.com>
2026-09-15 13:18:15 +0300

    Add try catch blocks

--- server/rateLimitService.ts
+++ server/rateLimitService.ts
@@ -8,55 +8,78 @@ import { RateLimitEntryModel } from "./models/rateLimitEntry";
 
 class RateLimitService implements Store {
     async increment(key: string): Promise<IncrementResponse> {
-        await db.connect();
+        try {
+            await db.connect();
 
-        const existingRateLimitEntry = await RateLimitEntryModel.findOne({ key: key }).exec();
+            const existingRateLimitEntry = await RateLimitEntryModel.findOne({ key: key }).exec();
 
-        if (!existingRateLimitEntry) {
-            const newRateLimitEntry = new RateLimitEntryModel({
-                key: key,
-                total_hits: 1,
-                reset_time_utc: new Date(),
+            if (!existingRateLimitEntry) {
+                const newRateLimitEntry = new RateLimitEntryModel({
+                    key: key,
+                    total_hits: 1,
+                    reset_time_utc: this.getResetTime(),
+                });
+                
+                const rateLimitEntry = await newRateLimitEntry.save();
+
+                return {
+                    totalHits: rateLimitEntry.total_hits,
+                    resetTime: rateLimitEntry.reset_time_utc,
+                }
+            }
+
+            const newTotalHits = existingRateLimitEntry.total_hits + 1;
+            await existingRateLimitEntry.updateOne({
+                total_hits: newTotalHits,
             });
-            
-            const rateLimitEntry = await newRateLimitEntry.save();
 
             return {
-                totalHits: rateLimitEntry.total_hits,
-                resetTime: rateLimitEntry.reset_time_utc,
+                totalHits: newTotalHits,
+                resetTime: existingRateLimitEntry.reset_time_utc,
             }
-        }
-
-        const newTotalHits = existingRateLimitEntry.total_hits + 1;
-        await existingRateLimitEntry.updateOne({
-            total_hits: newTotalHits,
-        });
-
-        return {
-            totalHits: newTotalHits,
-            resetTime: existingRateLimitEntry.reset_time_utc,
+        } catch {
+            return this.getDefaultIncrementResponse();
         }
     }
 
     async decrement(key: string): Promise<void> {
-        await db.connect();
+        try {
+            await db.connect();
 
-        const existingRateLimitEntry = await RateLimitEntryModel.findOne({ key: key }).exec();
-        if (!existingRateLimitEntry) {
+            const existingRateLimitEntry = await RateLimitEntryModel.findOne({ key: key }).exec();
+            if (!existingRateLimitEntry) {
+                return;
+            }
+
+            const newTotalHits = existingRateLimitEntry.total_hits - 1;
+            await existingRateLimitEntry.updateOne({
+                total_hits: newTotalHits,
+                reset_time_utc: new Date(),
+            });
+        } catch {
             return;
         }
-
-        const newTotalHits = existingRateLimitEntry.total_hits - 1;
-        await existingRateLimitEntry.updateOne({
-            total_hits: newTotalHits,
-            reset_time_utc: new Date(),
-        });
     }
 
     async resetKey(key: string): Promise<void> {
-        await db.connect();
+        try {
+            await db.connect();
 
-        await RateLimitEntryModel.deleteOne({ key: key }).exec();
+            await RateLimitEntryModel.deleteOne({ key: key }).exec();
+        } catch {
+            return;
+        }
+    }
+   
+    private getDefaultIncrementResponse(): IncrementResponse {
+        return {
+            totalHits: 1,
+            resetTime: this.getResetTime(),
+        };
+    }
+
+    private getResetTime(): Date {
+        return new Date();
     }
 }
 
