247832aad573d891c33fcd2c115f6f5646cb2997
Henri Vekuna <cde_1414@yahoo.com>
2026-09-15 08:29:40 +0300

    Key to index

--- server/schemas/rateLimitEntry.ts
+++ server/schemas/rateLimitEntry.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 export const rateLimitEntrySchema = new mongoose.Schema({
-    key: String,
+    key: { type: [String], index: true },
 
     total_hits: Schema.Types.Int32,
     reset_time: Date,
