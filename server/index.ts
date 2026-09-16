8ee4cfc5ef8590aee9567f7c4bce97fe05995c08
Henri Vekuna <cde_1414@yahoo.com>
2026-09-16 07:49:14 +0300

    Fix comment number

--- server/index.ts
+++ server/index.ts
@@ -35,7 +35,7 @@ app.use(helmet({
 // Rate limit
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 50, // Limit each IP to 300 requests per `window` (here, per 15 minutes)
+	limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes)
 	standardHeaders: true,
 	legacyHeaders: false,
 	ipv6Subnet: 56,
